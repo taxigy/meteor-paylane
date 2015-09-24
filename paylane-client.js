24048c50fa6adf1bcfc81e1caa7312f9383766d8
Rishat Muhametshin <rishatmuhametshin@gmail.com>
2015-09-24 13:28:43 +0400

    PayLane form and server Meteor.methods almost ready. `Paylane` object and methods for hash calculation ready.

--- paylane-client.js
+++ paylane-client.js
@@ -1,1 +1,45 @@
-// Write your package code here!
+/* global Meteor, Template */
+
+Template.paylaneCheckout.helpers({
+    amount: function () {
+    },
+    currency: function () {
+    },
+    merchantId: function () {
+    },
+    description: function (text) {
+        var description = String(text);
+
+        return description.replace(/[^a-zA-Z0-9]/gi, '');
+    },
+    transactionDescription: function () {
+    },
+    transactionType: function (type) {
+        if (!type) {
+            return 'S';
+        } else {
+            return type;
+        }
+    },
+    backUrl: function (text) {
+        var url = text || Meteor.settings.public.appRootUrl || '';
+
+        return url;
+    },
+    language: function () {
+    },
+    hash: function () {
+        var options = {};
+
+        options.description = this.description;
+        options.amount = this.amount;
+        options.currency = this.currency;
+        options.transaction_type = this.transactionType;
+        
+        return ReactiveMethod.call('calculateHash', options);
+    },
+    buttonText: function () {
+        return 'Submit';
+    }
+});
+
---
+++ paylane-server.js
@@ -0,0 +1,48 @@
+Paylane = function () {};
+
+Paylane.calculateHash = function (options) {
+    var text;
+
+    if (!options) {
+        throw new Meteor.Error(500, 'Must provide options to calculate hash for PayLane checkout form.');
+    }
+
+    if (!options.description) {
+        throw new Meteor.Error(500, 'Description is mandatory for PayLane checkout.');
+    }
+
+    if (!options.amount) {
+        throw new Meteor.Error(500, 'Amount is mandatory for PayLane checkout.');
+    }
+
+    if (!options.currency) {
+        throw new Meteor.Error(500, 'Currency is mandatory for PayLane checkout');
+    }
+
+    if (!options.transaction_type && !options.status) {
+        throw new Meteor.Error(500, 'Something is wrong with options. Are these pre-checkout form or post-checkout callback options?');
+    }
+
+    if (!Meteor.settings.paylane.salt) {
+        throw new Meteor.Error(500, 'Cannot calculate hash for PayLane checkout without hash salt.');
+    }
+
+    text = [
+        Meteor.settings.paylane.salt,
+        options.status || '',
+        String(options.amount),
+        String(options.description).replace(/[^a-zA-Z0-9]/gi, ''),
+        options.currency,
+        options.id_sale || options.id_authorization,
+        options.transaction_type
+    ].join('|').replace(/\|+/gi, '|').replace(/(^\|+|\|+$)/gi, '');
+
+    return CryptoJS.SHA1(text).toString();
+};
+
+Meteor.methods({
+    calculateHash: function (options) {
+        return Paylane.calculateHash(options);
+    }
+});
+
